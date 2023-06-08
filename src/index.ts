bb21b5446255814d263c7a6efb4b866d727e4636
Sekhara Pramod <32711469+V-Volte@users.noreply.github.com>
2023-06-08 19:43:36 +0530

    Minor changes to publish to npm

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import Language from "./core/Language";
+import LanguageConstructor from "./core/LanguageConstructor";
+import InflectionPattern from "./core/InflectionPattern";
+import InflectionClass from "./core/inflectionclasses/InflectionClass";
+import VerbInflectionClass from "./core/inflectionclasses/VerbInflectionClass";
+import NounInflectionClass from "./core/inflectionclasses/NounInflectionClass";
+
+export { Language, LanguageConstructor, InflectionPattern, InflectionClass, VerbInflectionClass, NounInflectionClass };
